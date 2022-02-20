cf8e2ab14dd029f9be3fd3743ce5a48c5ab4690c
Hyerin Kim <hkim124@hawk.iit.edu>
2022-02-20 19:18:33 +0900

    [NestJS] TypeORM을 이용해 DB 연결 (mysql)
    - typeORM-model-generator를 이용해 DB데이터를 nest 엔티티로 추출
    - app.module.ts에서 ormconfig를 참고해 DB 계정 정보 확보 및 연결 수립

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -1,7 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
@@ -10,10 +11,32 @@ import { DmsController } from './dms/dms.controller';
 import { ChannelsController } from './channels/channels.controller';
 import { ChannelsService } from './channels/channels.service';
 import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: ['entities/*.js'],
+          autoLoadEntities: true, //TypeOrmModule.forFeature에 든 엔티티만 자동으로 연결
+          synchronize: true,
+          // 처음 생성할 때 sync 맞추기 위해 사용하고, 이후에는 안정성을 위해 false로 두는 것을 권장
+          logging: true, // ORM이 어떤 sql로 쿼리를 날렸는지 알 수 있음
+          keepConnectionAlive: true, // hot reloading을 위해 계속 connection 유지
+        };
+      },
+    }),
+    UsersModule,
+    WorkspacesModule,
+  ],
   controllers: [AppController, DmsController, ChannelsController],
   providers: [AppService, DmsService, ChannelsService, UsersService],
 })
